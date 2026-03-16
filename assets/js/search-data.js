a3cb27b1d5a7ae333ed145548754c8e0f0a3a7c0
gaurav-gaurav <88543029+gaurav-gaurav@users.noreply.github.com>
2026-03-16 03:46:59 +0000

    Deploying to gh-pages from @ gaurav-gaurav/gaurav-gaurav.github.io@4c0d72634e0dbf8faf833cd1c762d722ee1b5852 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,7 +49,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-paper-has-been-accepted-for-publication-in-tmlr",
+            },},{id: "news-one-paper-has-been-accepted-for-publication-in-irc-2023",
+          title: 'One paper has been accepted for publication in IRC,2023.',
+          description: "",
+          section: "News",},{id: "news-a-paper-has-been-accepted-for-publication-in-tmlr",
           title: 'A paper has been accepted for publication in TMLR.',
           description: "",
           section: "News",},{id: "news-presented-the-open-seminar-and-submitted-the-thesis",
